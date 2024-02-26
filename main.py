f9c93ace3e80d8894ccc8d4ac051118289649dca
juhigulati5 <158093742+juhigulati5@users.noreply.github.com>
2024-02-26 11:59:23 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,34 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def caesar(start_text, shift_amount, cipher_direction):
+  end_text = ""
+  if cipher_direction == "decode":
+    shift_amount *= -1
+  for char in start_text:
+    if char not in alphabet:
+      end_text += char
+    else:
+      position = alphabet.index(char)
+      new_position = position + shift_amount
+      if new_position > 25:
+        new_position -= 26
+      end_text += alphabet[new_position]
+
+  print(f"Here's the {cipher_direction}d result: {end_text}")
+
+from art import logo
+print(logo)
+
+
+keep_playing = True
+
+while keep_playing:
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+  
+  caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+  go_again = input("Do you want to start the program again? Type yes or no.\n")
+  if go_again == "no":
+    keep_playing = False
+    print("Goodbye.")
